041a2db2bf5e13ee3f7c28494e461ea7dffb4463
GachiLord <name504172@gmail.com>
2025-06-06 14:59:08 +0500

    add telescope to nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -26,6 +26,9 @@ vim.api.nvim_create_autocmd({'FileType'}, {
 -- Set tree-view in file manager 
 vim.g.netrw_liststyle = 3
 
+-- set leader
+vim.g.mapleader = " "
+
 -- Add tabs mappings
 vim.api.nvim_set_keymap('n', '<C-H>', ':tabprev <CR>', {})
 vim.api.nvim_set_keymap('n', '<C-L>', ':tabnext <CR>', {})
@@ -33,4 +36,11 @@ vim.api.nvim_set_keymap('n', '<C-L>', ':tabnext <CR>', {})
 -- Add buffer mappings
 vim.api.nvim_set_keymap('n', '<C-J>', ':bp <CR>', {})
 vim.api.nvim_set_keymap('n', '<C-K>', ':bn <CR>', {})
+
+-- telescope
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>g', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = 'Telescope buffers' })
 
