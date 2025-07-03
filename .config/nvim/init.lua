1398ee49200f6d583b0441d4acccbabe20f3578f
GachiLord <name504172@gmail.com>
2025-07-03 20:26:18 +0500

    add nvim key-binding

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -65,8 +65,8 @@ vim.api.nvim_set_keymap('n', ',', ':noh <CR>', {})
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>g', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>s', builtin.grep_string, { desc = 'Telescope grep string' })
 vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = 'Telescope buffers' })
-
 
 -- marks
 
